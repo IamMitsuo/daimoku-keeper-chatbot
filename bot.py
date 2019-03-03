be032e6b8b2598a456ab419303da7bee0aaf69f4
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2019-03-04 03:39:17 +0700

    Update bot.py and requirements.txt

--- bot.py
+++ bot.py
@@ -1,5 +1,28 @@
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+    ButtonsTemplate, URITemplateAction, PostbackTemplateAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
+)
+from boto.s3.connection import S3Connection
+
+LINE_CHANNEL_SECRET = os.environ['LINE_CHANNEL_SECRET']
+LINE_CHANNEL_ID = os.environ['LINE_CHANNEL_ID']
+LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+
 app = Flask(__name__)
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 @app.route("/")
 def hello():
@@ -7,7 +30,27 @@ def hello():
 
 @app.route("/webhook", methods=['GET', 'POST'])
 def webhook():
+    # Get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # Get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # Handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError
+        abort(400)
+
     return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
 
 if __name__ == "__main__":
     app.run()
