3eecbd8d834c90798796b734795b24e29d6a055b
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2019-03-04 03:20:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
-    
+
+@app.route("/webhook", methods=['GET', 'POST'])
+def webhook():
+    return 'OK'
+
 if __name__ == "__main__":
     app.run()
