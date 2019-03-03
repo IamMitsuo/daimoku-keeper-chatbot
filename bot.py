f65b6169ea58c82ba18c1353f2b37459406d65ae
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2019-03-04 04:40:33 +0700

    Add GCS Keyfile

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -18,6 +19,17 @@ from linebot.models import (
 LINE_CHANNEL_SECRET = os.environ['LINE_CHANNEL_SECRET']
 LINE_CHANNEL_ID = os.environ['LINE_CHANNEL_ID']
 LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+GCS_KEYFILE = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+GCS_KEYFILE_PATH = ''
+
+if not os.path.exists(GCS_KEYFILE):
+    # Create file path for GCS by parsing the string to json file
+    print('here')
+    GCS_KEYFILE_PATH = os.path.join(os.getcwd(), 'gcs_keyfile.json')
+    with open(GCS_KEYFILE_PATH, 'w') as gcs_keyfile_file:
+        json.dump(json.loads(GCS_KEYFILE), gcs_keyfile_file, indent=4)
+        gcs_keyfile_file.close()
+
 
 app = Flask(__name__)
 
@@ -47,10 +59,24 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+    text = event.message.text
+
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=event.message.text)
+    # )
+
+def explicit():
+    from google.cloud import storage
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(
+        GCS_KEYFILE_PATH)
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
 
 if __name__ == "__main__":
     app.run()
