6585f0fea48f5efb7758df7c1d9809b60811fa11
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2019-03-04 20:18:05 +0700

    Fix firebase code

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import json
 import uuid
+import datetime, pytz, dateutil.parser
 from flask import Flask, request, abort, Response, jsonify
 from linebot import (
     LineBotApi, WebhookHandler
@@ -17,30 +18,51 @@ from linebot.models import (
     UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
 )
 from google.protobuf.json_format import MessageToDict, MessageToJson
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from google.cloud import exceptions
 
 LINE_CHANNEL_SECRET = os.environ['LINE_CHANNEL_SECRET']
 LINE_CHANNEL_ID = os.environ['LINE_CHANNEL_ID']
 LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
-GCS_KEYFILE = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
-GCS_KEYFILE_PATH = ''
+GCS_DIALOGFLOW_KEYFILE = os.environ['GOOGLE_APPLICATION_CREDENTIALS_DIALOGFLOW']
+GCS_DIALOGFLOW_KEYFILE_PATH = ''
 DIALOGFLOW_PROJECT_ID = 'daimoku-keeper'
 DIALOGFLOW_SESSION_ID = uuid.uuid4().hex
+GCS_FIREBASE_ADMINSDK_KEYFILE = os.environ['GOOGLE_APPLICATION_CREDENTIALS_FIREBASE_ADMINSDK']
+GCS_FIREBASE_ADMINSDK_KEYFILE_PATH = ''
 
-if not os.path.exists(GCS_KEYFILE):
+if not os.path.exists(GCS_DIALOGFLOW_KEYFILE):
     # Create file path for GCS by parsing the string to json file
-    print('here')
-    GCS_KEYFILE_PATH = os.path.join(os.getcwd(), 'gcs_keyfile.json')
-    with open(GCS_KEYFILE_PATH, 'w') as gcs_keyfile_file:
-        json.dump(json.loads(GCS_KEYFILE), gcs_keyfile_file, indent=4)
+    GCS_DIALOGFLOW_KEYFILE_PATH = os.path.join(os.getcwd(), 'gcs_dialogflow_keyfile.json')
+    with open(GCS_DIALOGFLOW_KEYFILE_PATH, 'w') as gcs_keyfile_file:
+        json.dump(json.loads(GCS_DIALOGFLOW_KEYFILE), gcs_keyfile_file, indent=4)
         gcs_keyfile_file.close()
 else:
-    GCS_KEYFILE_PATH = GCS_KEYFILE
+    GCS_DIALOGFLOW_KEYFILE_PATH = GCS_DIALOGFLOW_KEYFILE
+
+if not os.path.exists(GCS_FIREBASE_ADMINSDK_KEYFILE):
+    # Create file path for GCS by parsing the string to json file
+    GCS_FIREBASE_ADMINSDK_KEYFILE_PATH = os.path.join(os.getcwd(), 'gcs_firebase_adminsdk_keyfile.json')
+    with open(GCS_FIREBASE_ADMINSDK_KEYFILE_PATH, 'w') as gcs_keyfile_file:
+        json.dump(json.loads(GCS_FIREBASE_ADMINSDK_KEYFILE), gcs_keyfile_file, indent=4)
+        gcs_keyfile_file.close()
+else:
+    GCS_FIREBASE_ADMINSDK_KEYFILE_PATH = GCS_FIREBASE_ADMINSDK_KEYFILE
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+cred = credentials.Certificate(GCS_FIREBASE_ADMINSDK_KEYFILE_PATH)
+firebase_app = firebase_admin.initialize_app(cred)
+print(firebase_app.project_id)
+db = firestore.client(app=firebase_app)
+print(db)
+print(firebase_admin.get_app().credential)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
@@ -79,6 +101,15 @@ def test_dialogflow():
 def test_explicit():
     return explicit()
 
+@app.route("/test/firestore", methods=['GET'])
+def test_firestore():
+    users_ref = db.collection(u'users')
+    docs = users_ref.get()
+    doc_list = []
+    for doc in docs:
+        doc_list.append(doc.to_dict())
+    return str(doc_list)
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
@@ -91,17 +122,37 @@ def handle_message(event):
         if intentName == 'RequestToKeepDaimokuIntent':
             parameters = MessageToDict(detected_intent.query_result.parameters)
             daimoku_count = None
+            added_datetime = datetime.datetime.now(tz=pytz.timezone('Asia/Bangkok')).isoformat()
+
             if parameters['DaimokuCountEntryEntity'] != '' and parameters['DaimokuCountEntryEntity1'] != '':
                 daimoku_count = int(parameters['DaimokuCountEntryEntity']['number'] + parameters['DaimokuCountEntryEntity1']['number'])
             elif parameters['DaimokuCountEntryEntity'] != '':
                 daimoku_count = int(parameters['DaimokuCountEntryEntity']['number'])
             elif parameters['DaimokuCountEntryEntity1'] != '':
                 daimoku_count = int(parameters['DaimokuCountEntryEntity']['number'])
-            text = 'รับทราบครับ {} สวดได้ {} ช่อง'.format(source_userId, daimoku_count)
+
+            if parameters['date'] != '':
+                added_datetime = parameters['date']
+                added_datetime_obj = dateutil.parser.parse(added_datetime)
+                added_datetime_obj = added_datetime_obj.replace(year=datetime.datetime.now(tz=pytz.timezone('Asia/Bangkok')).year)
+                added_datetime = added_datetime_obj.isoformat()
+            try:
+                user_doc = db.collection(u'users').document(u'{}'.format(source_userId)).get()
+                user_doc_dict = user_doc.to_dict()
+                db.collection(u'daimokuLog').add({
+                    u'count': daimoku_count,
+                    u'date': added_datetime,
+                    u'user': source_userId
+                })   
+                text = 'รับทราบครับ {} สวดได้ {} ช่อง ({})'.format(user_doc_dict['name'], daimoku_count, added_datetime)
+            except exceptions.NotFound:
+                text = 'ขอชื่อด้วยคร้าบ'
+
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=text)
             )
+
         elif intentName == 'Default Fallback Intent':
             line_bot_api.reply_message(
                 event.reply_token,
@@ -129,7 +180,7 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
     of the conversation."""
 
     import dialogflow_v2 as dialogflow
-    session_client = dialogflow.SessionsClient.from_service_account_json(GCS_KEYFILE_PATH)
+    session_client = dialogflow.SessionsClient.from_service_account_json(GCS_DIALOGFLOW_KEYFILE_PATH)
 
     session = session_client.session_path(project_id, session_id)
     print('Session path: {}\n'.format(session))
